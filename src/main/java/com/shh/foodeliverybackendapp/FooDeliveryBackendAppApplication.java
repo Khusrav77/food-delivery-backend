1be2a5cb641ed081ceeb3bfe740642441b1d3a77
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2026-05-11 23:21:14 +0300

    Initial commit

---
+++ src/main/java/com/shh/foodeliverybackendapp/FooDeliveryBackendAppApplication.java
@@ -0,0 +1,14 @@
+package com.shh.foodeliverybackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooDeliveryBackendAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooDeliveryBackendAppApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/test/java/com/shh/foodeliverybackendapp/FooDeliveryBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shh.foodeliverybackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooDeliveryBackendAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
